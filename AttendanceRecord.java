049d447e6b225c8275d6430e4708df9114cf510b
Vettas Alexandros <alexandros.vettas@gmail.com>
2025-07-22 13:25:59 +0300

    3 Entities Created
    -Student.java
    -Course.java
    -AttendanceRecord.java

---
+++ AttendanceRecord.java
@@ -0,0 +1,10 @@
+public class AttendanceRecord {
+    public static void AttendanceRecord (String[] args) {
+        String attendanceRecordId;
+        String attendanceRecordStudentId;
+        String attendanceRecordCourseId;
+        String attendanceRecordDate;
+        boolean present;
+    }
+    
+}
---
+++ Course.java
@@ -0,0 +1,9 @@
+public class Course {
+    public static void Course(String[] args) {
+
+         String courseId;
+         String courseName;
+         String courseInstructor;
+    }
+    
+}
