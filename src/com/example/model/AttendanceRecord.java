56821f7be006dff8e784f681de4edd201ba03eef
Vettas Alexandros <alexandros.vettas@gmail.com>
2025-07-24 19:48:36 +0300

    Servlets, Getters & Setters
    -Getters Setters beautified
    -Implemented CoursesServlet.java
    -Implemented AttendanceRecordServlet.java

--- src/com/example/model/AttendanceRecord.java
+++ src/com/example/model/AttendanceRecord.java
@@ -1,11 +1,27 @@
 package com.example.model;
+import java.time.LocalDate;
+
 public class AttendanceRecord {
-    public static void AttendanceRecord (String[] args) {
-        String attendanceRecordId;
-        String attendanceRecordStudentId;
-        String attendanceRecordCourseId;
-        String attendanceRecordDate;
-        boolean present;
-    }
-    
+    private int id;
+    private int studentId;
+    private int courseId;
+    private LocalDate date;
+    private boolean present;
+
+    // getters&setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getStudentId() { return studentId; }
+    public void setStudentId(int studentId) { this.studentId = studentId; }
+
+    public int getCourseId() { return courseId; }
+    public void setCourseId(int courseId) { this.courseId = courseId; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public boolean isPresent() { return present; }
+    public void setPresent(boolean present) { this.present = present; }
 }
+
--- src/com/example/model/Course.java
+++ src/com/example/model/Course.java
@@ -7,34 +7,16 @@ public class Course {
     private String courseName;
     private String courseInstructor;
 
-    public Course() {
-    }
+    
 
-    //getters
-    public int getCourseId() {
-        return courseId;
-    }
+    public int getCourseId() {return courseId;}
+    public void setCourseId(int courseId) {this.courseId = courseId;}
 
-    public void setCourseId(int courseId) {
-        this.courseId = courseId;
-    }
+    public String getCourseName() {return courseName;}
+    public void setCourseName(String courseName) {this.courseName = courseName;}
 
-    public String getCourseName() {
-        return courseName;
-    }
-
-    //setters
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public String getCourseInstructor() {
-        return courseInstructor;
-    }
-
-    public void setCourseInstructor(String courseInstructor) {
-        this.courseInstructor = courseInstructor;
-    }
+    public String getCourseInstructor() {return courseInstructor;}
+    public void setCourseInstructor(String courseInstructor) {this.courseInstructor = courseInstructor;}
     
     
 
